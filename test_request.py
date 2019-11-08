eaf3e2c385b3b40dcf04be71fcd0f26ce586e1b4
russell-pollari <pollarir@mgail.com>
2019-11-08 11:53:55 -0500

    Remove unused file

--- test_request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-file = open('data/sample/metal/0C3puNFNfpDwVHEZNU4Shx.jpg')
-
-response = requests.post('http://localhost:5000/predict', files={ "file": file })
-
-print(response.json())
