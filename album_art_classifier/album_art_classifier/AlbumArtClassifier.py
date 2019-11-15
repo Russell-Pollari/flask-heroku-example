2e10007c9837fc8957b47b57cd238aa2c8741235
russell-pollari <pollarir@mgail.com>
2019-11-15 11:22:12 -0500

    Add test for prediction

--- album_art_classifier/album_art_classifier/AlbumArtClassifier.py
+++ album_art_classifier/album_art_classifier/AlbumArtClassifier.py
@@ -2,12 +2,12 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-from album_art_classifier.model import model
+from album_art_classifier.model import model as _model
 
 
 class AlbumArtClassifier:
 	def __init__(self):
-		self.model = model
+		self.model = _model
 
 	def load_weights(self, file):
 		self.model = load_model(file)
@@ -17,6 +17,6 @@ class AlbumArtClassifier:
 
 		pred = self.model.predict(np.expand_dims(x, axis=0))
 
-		genre = 'metal!' if pred[0][0] < 0.5 else 'not metal'
+		genre = 'metal' if pred[0][0] < 0.5 else 'not_metal'
 
 		return genre, pred[0][0]
--- album_art_classifier/tests/test_predict.py
+++ album_art_classifier/tests/test_predict.py
@@ -1,4 +1,13 @@
-def test():
-	assert 1 == 1
+from album_art_classifier import AlbumArtClassifier
 
-# TODO: write some actual tests
+
+def test_predict():
+	test_image_path = 'data/sample/metal/0C3puNFNfpDwVHEZNU4Shx.jpg'
+
+	classifier = AlbumArtClassifier()
+	classifier.load_weights('trained_models/model.h5')
+
+	genre, pred = classifier.classify_one(test_image_path)
+
+	assert genre in ['metal', 'not_metal']
+	assert pred is not None
