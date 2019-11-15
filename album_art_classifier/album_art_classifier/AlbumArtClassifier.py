63187eeba1ec662b42651aa55389059f299456d5
russell-pollari <pollarir@mgail.com>
2019-11-15 11:59:20 -0500

    Add tests for training model

--- album_art_classifier/album_art_classifier/AlbumArtClassifier.py
+++ album_art_classifier/album_art_classifier/AlbumArtClassifier.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 from album_art_classifier.model import model as _model
+from album_art_classifier.train_model import train_model
 
 
 class AlbumArtClassifier:
@@ -11,6 +12,13 @@ class AlbumArtClassifier:
 
 	def load_weights(self, file):
 		self.model = load_model(file)
+
+	def fit_from_directory(self, directory, save_path):
+		self.model = train_model(
+			self.model,
+			data_folder=directory,
+			save_path=save_path
+		)
 
 	def classify_one(self, file):
 		x = img_to_array(load_img(file)) / 255
--- album_art_classifier/tests/test_train.py
+++ album_art_classifier/tests/test_train.py
@@ -1,2 +1,12 @@
-def test():
-	assert 1 == 1
+import os
+from album_art_classifier import AlbumArtClassifier
+
+
+def test_train(tmp_path):
+	test_directory = 'data/sample'
+	save_path = tmp_path / 'test_model.h5'
+
+	classifier = AlbumArtClassifier()
+	classifier.fit_from_directory(test_directory, save_path)
+
+	assert os.path.exists(save_path)
