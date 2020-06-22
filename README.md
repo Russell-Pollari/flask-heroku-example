767809513b9f1caddc187588a0285eef5d892833
russell-pollari <pollarir@mgail.com>
2020-06-22 17:16:49 -0400

    Remore S3 dependency..

--- README.md
+++ README.md
@@ -85,7 +85,6 @@ $ python scripts/run_experiment.py experiment_name \
 
 
 ## Running Flask app locally
-
 To ensure app runs in debug mode set `FLASK_ENV` envioronment variable before running:  
 `$ export FLASK_ENV=development`
 
@@ -94,9 +93,6 @@ To run app locally (starts server on localhost:8000)
 
 
 ## Deploying:
-
 1. Create heroku account and install heroku CLI: https://devcenter.heroku.com/articles/heroku-cli#download-and-install
 2. `$ heroku create <app_name>`
-3. Set `S3_URL` env variable (points to saved weights)  
-`$ heroku config:set S3_URL=<url>`  
 4. Deploy: `$ git push heroku master`
--- album_art_classifier/album_art_classifier/load_model.py
+++ album_art_classifier/album_art_classifier/load_model.py
@@ -1,23 +1,8 @@
-import tempfile
-import wget
-import os
-
 from album_art_classifier.AlbumArtClassifier import AlbumArtClassifier
 
 
-def load_model(from_s3=True):
+def load_model(path_to_model='trained_models/model.h5'):
+	print('Loading model from file system...')
 	classifer = AlbumArtClassifier()
-
-	if from_s3:
-		s3_url = os.getenv('S3_URL')
-		with tempfile.TemporaryDirectory() as tmpdir:
-			print('Fetching weights from S3', s3_url)
-			wget.download(s3_url, '{}/weights.h5'.format(tmpdir))
-
-			print('Loading weights...')
-			classifer.load_weights('{}/weights.h5'.format(tmpdir))
-	else:
-		print('Loading model from file system...')
-		classifer.load_weights('trained_models/model.h5')
-
+	classifer.load_weights(path_to_model)
 	return classifer
